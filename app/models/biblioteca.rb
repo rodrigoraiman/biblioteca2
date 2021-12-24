e179a54461493952551cc540b6f1e84afb067846
rodrigoraiman <rodrigoraiman@gmail.com>
2021-12-24 13:09:38 -0300

    avances

--- app/models/biblioteca.rb
+++ app/models/biblioteca.rb
@@ -2,6 +2,8 @@ class Biblioteca < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
     validates :status, presence: true
+
+    paginates_per 5
    
    
 end
